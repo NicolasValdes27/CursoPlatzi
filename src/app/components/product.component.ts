1ed5c61e8e2988cfaa1daf8363ce030cc9a385d0
Nicolas <nicolasignacio.vq@gmail.com>
2020-11-20 21:26:01 -0300

    e

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,16 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnInit, DoCheck, OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
 })
 
 
-export class ProductComponent{
+export class ProductComponent implements OnInit, DoCheck, OnDestroy{
 
     @Input() product!: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor(){
+        console.log('constructor');
+    }
+
+    // ngOnChanges(changes: SimpleChanges): void{
+      //  console.log('ngOnChanges');
+        // console.log(changes);
+    // }
+
+    ngOnInit(): void {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck(): void{
+        console.log('4. ngDoCheck');
+    }
+
+    ngOnDestroy(): void{
+        console.log('5. ngOnDestroy');
+    }
 
     addCart(): void{
         console.log('Añadir al carrito');
